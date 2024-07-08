6ec1d20f64024e43d40b61ef7c978d73e43266bf
William Ryan O'Sullivan <wrosullivan88@gmail.com>
2024-07-08 16:37:58 -0500

    idk who cares

--- routes/hello.js
+++ routes/hello.js
@@ -1,5 +1,29 @@
 'use strict'
 
+// import pino from 'pino';
+// import { Logger } from 'pino';
+
+// const logTest = pino()
+
+// // const server = fastify({
+// //   logger: logTest
+// // })
+
+// logTest.info('hello world')
+// logTest.error('this is at error level  testeststestestestests')
+
+
+
+// const logger: Logger = pino({
+//     level: 'info' as const,
+//     transport: {
+//       target: 'pino-pretty',
+//       options: {
+//         colorize: true
+//       }
+//     }
+//   });
+
 module.exports = async function (fastify, opts) {
     fastify.get('/hello', async function (request, reply) {
         return {message: 'Hello, Buttface!'}
